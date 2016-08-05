ffdde397c29abbc66f16445e825c93977bbf45b9
Draldo <draldo_lopez@hotmail.com>
2016-08-05 14:45:29 -0400

    async okhttp

--- app/src/main/java/com/example/admin/asynchronousokhttp/MainActivity.java
+++ app/src/main/java/com/example/admin/asynchronousokhttp/MainActivity.java
@@ -1,15 +1,49 @@
 package com.example.admin.asynchronousokhttp;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String url = "http://www.mocky.io/v2/57a01bec0f0000c10d0f650f";
+    private static final String TAG = MainActivity.class.getSimpleName() + "TAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void doMagic(View view) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+
+            //Response response = client.newCall(request).execute(); --> synchronously
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, "onResponse: " + response.body().string());
+            }
+        });
 
     }
 }
